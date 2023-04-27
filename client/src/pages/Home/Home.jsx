6aad53b028a83e098407d4972f3ca3ef12e06af4
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-04-27 18:40:06 -0300

    feat: add calendar

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Calendar from "../../components/Calendar/Calendar";
 import AddEvent from "../../components/AddEvent/AddEvent";
+import home from './Home.module.css'
 
 // Home con todos sus componentes
 
 const Home = () => {
   return (
-    <div>
+    <div className={home.container}>
       <div>Estoy en el Home perro </div>
       <div>
         <AddEvent />
