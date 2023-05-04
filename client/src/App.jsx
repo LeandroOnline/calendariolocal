6f200acd941d7ceba63c7b37523fc77603763539
LeandroOnline <105742869+LeandroOnline@users.noreply.github.com>
2023-05-04 16:15:08 -0300

    feat: new structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import "./reset.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import "./reset.css";
 import "dayjs/locale/es";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import CalendarPage from "./pages/CalendarPage/CalendarPage";
+import Oficios from "./pages/Oficios/Oficios"
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
           <Navbar />
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/calendar" element={<CalendarPage />} />
+            <Route exact path="/oficios" element={<Oficios />} />
           </Routes>
         </BrowserRouter>
       </LocalizationProvider>
