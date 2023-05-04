b0e3093497c4d8e5303f48510e0f915e52ab681a
EliseoRom <romeroeliseojoaquin@gmail.com>
2023-05-04 13:16:38 -0300

    Add: Calendar controller

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,10 +1,13 @@
-
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
 
 const Calendar = () => {
+  const [value, setValue] = useState(dayjs("2022-04-17"));
+
   return (
     <>
-      <DateCalendar />
+      <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
     </>
   );
 };
