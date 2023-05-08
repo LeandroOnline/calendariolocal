97c60701deba102a62f70f996f9c3ed95ea5389b
EliseoRom <romeroeliseojoaquin@gmail.com>
2023-05-08 17:44:28 -0300

    Add: DatePicker components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ConfigProvider } from "antd";
+import es_ES from "antd/locale/es_ES";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ConfigProvider locale={es_ES}>
     <App />
-  </React.StrictMode>,
-)
+  </ConfigProvider>
+);
