7b0870dfc5e25acf3847c14997377f4466de7af2
EliseoRom <romeroeliseojoaquin@gmail.com>
2023-05-08 14:47:08 -0300

    Deleted: materia

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 // navbar
 import React from "react";
 import { Link } from "react-router-dom";
+// prueba date picjer antd
+import { DatePicker } from "antd";
+//import "antd/dist/antd.css";
 
 const Navbar = () => {
   return (
     <div>
-      <Link to='/'>Home</Link>
-      <Link to='/oficios'>Oficios</Link>
-      <Link to='/calendar'>Calendario</Link>
+      <p>Esta es la prueba AntDesign</p>
+      <DatePicker />
+      <Link to="/">Home</Link>
+      <Link to="/oficios">Oficios</Link>
+      <Link to="/calendar">Calendario</Link>
     </div>
   );
 };
