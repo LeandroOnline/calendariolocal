ff7308f3385a7a7757df4807dfebb3009b075efc
EliseoRom <romeroeliseojoaquin@gmail.com>
2023-05-08 15:12:27 -0300

    feat: stable version v1

--- client/src/components/Calendario/AddEvent/AddEvent.jsx
+++ client/src/components/Calendario/AddEvent/AddEvent.jsx
@@ -1,7 +1,6 @@
 import API from "../../../assets/API/api";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { DatePicker, StaticTimePicker } from "@mui/x-date-pickers";
 
 const AddEvent = () => {
   const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
@@ -10,47 +9,12 @@ const AddEvent = () => {
   const [valueEnd, setValueEnd] = useState(null);
   const [time, setTime] = useState(null);
 
-  const addEvent = async () => {
-    await axios.post(API + "/addevent", newEvent).then((response) => {
-      getEvents();
-    });
-  };
+  // const addEvent = async () => {
+  //   await axios.post(API + "/addevent", newEvent).then((response) => {
+  //     getEvents();
+  //   });
+  // };
 
-  const getEvents = async () => {
-    await axios.get(API + "/getevents").then((response) => {
-      const alldata = response.data;
-      let dataparser = [];
-      alldata.map((data) => {
-        dataparser.push({
-          title: data.title,
-          start: new Date(Date.parse(data.start)),
-          end: new Date(Date.parse(data.end)),
-        });
-      });
-      setAllEvents(dataparser);
-    });
-  };
-
-  useEffect(() => {
-    getEvents();
-  }, []);
-
-  return (
-    <>
-      <DatePicker
-        value={valueStart}
-        onChange={(newValueStart) => setValue(newValueStart)}
-      />
-      <DatePicker
-        value={valueEnd}
-        onChange={(newValueEnd) => setValue(newValueEnd)}
-      />
-      <StaticTimePicker
-        orientation="landscape"
-        value={time}
-        onChange={(time) => setTime(time)}
-      />
-    </>
-  );
+  return <></>;
 };
 export default AddEvent;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,14 @@
 // navbar
 import React from "react";
 import { Link } from "react-router-dom";
-// prueba date picjer antd
-import { DatePicker } from "antd";
-//import "antd/dist/antd.css";
+
+
 
 const Navbar = () => {
   return (
     <div>
-      <p>Esta es la prueba AntDesign</p>
-      <DatePicker />
+   
       <Link to="/">Home</Link>
-      <Link to="/oficios">Oficios</Link>
       <Link to="/calendar">Calendario</Link>
     </div>
   );
