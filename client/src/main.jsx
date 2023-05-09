205ee46731b16e04f7f1059ca7cb5714abdc9c9b
Leandro Savat <leandrosavat@MacBook-Air-de-Leandro.local>
2023-05-09 18:31:04 -0300

    feat: update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./routes/App";
+import App from "./routes/App.jsx";
 import { ConfigProvider } from "antd";
 import es_ES from "antd/locale/es_ES";
 import "./reset.css";
