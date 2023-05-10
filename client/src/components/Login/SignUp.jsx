9f095715b384ca7e2c9d5262f7ccc50564133aa6
EliseoRom <romeroeliseojoaquin@gmail.com>
2023-05-10 18:06:08 -0300

    add: form register

--- client/src/components/Login/SignUp.jsx
+++ client/src/components/Login/SignUp.jsx
@@ -1,1 +1,168 @@
+import { Button, Checkbox, Form, Input, Select } from "antd";
+import { useState } from "react";
+
 // Incribirse crear cuenta nueva
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+const Signup = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{
+        residence: ["zhejiang", "hangzhou", "xihu"],
+        prefix: "86",
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="nickname"
+        label="Nombre"
+        tooltip="Nombre completo"
+        rules={[
+          {
+            required: true,
+            message: "Ingresa tu nombre",
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="nickname"
+        label="Apellido"
+        tooltip="Se debe añadir apellido"
+        rules={[
+          {
+            required: true,
+            message: "Ingresa tu apellido",
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: "email",
+            message: "Debe ingresar un E-mail valido",
+          },
+          {
+            required: true,
+            message: "Ingresa tu E-mail",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Contraseña"
+        rules={[
+          {
+            required: true,
+            message: "Ingrese su contraseña",
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirmar contraseña"
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Confirma tu contraseña",
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(
+                new Error("The two passwords that you entered do not match!")
+              );
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value
+                ? Promise.resolve()
+                : Promise.reject(
+                    new Error("Debe aceptar la confirmacion de datos")
+                  ),
+          },
+        ]}
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          Confirmar mis datos <a href=""></a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Registrarme
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+export default Signup;
