be24daa750db8e9aa916c9892c296eacb8190686
EliseoRom <romeroeliseojoaquin@gmail.com>
2023-05-10 15:54:13 -0300

    add: Page and rutes signup navbar conection

--- client/src/routes/App.jsx
+++ client/src/routes/App.jsx
@@ -3,9 +3,9 @@ import Navbar from "../components/Navbar/Navbar";
 import Home from "../pages/Home/Home";
 import CalendarPage from "../pages/CalendarPage/CalendarPage";
 import NotFound from "../pages/404/NotFound";
-import Login from "../pages/LoginPage/LoginPage"
 import Dashboard from "../pages/dashboard/Dashboard";
-
+import Login from "../pages/LoginPage/LoginPage";
+import SignUpPage from "../pages/LoginPage/SignUpPage";
 
 const App = () => {
   return (
@@ -16,6 +16,7 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/dash" element={<Dashboard />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUpPage />} />
           <Route exact path="/calendar" element={<CalendarPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
